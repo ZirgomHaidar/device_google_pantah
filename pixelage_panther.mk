04f5399d85e00943d8fa3b69db79cd617e5bb85c
ZirgomHaidar <zirgomhaidar17@gmail.com>
2025-03-19 13:21:23 +0530

    BringUp For Pixelage

--- pixelage_panther.mk
+++ pixelage_panther.mk
@@ -6,28 +6,28 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
 # Inherit device configuration
-DEVICE_CODENAME := cheetah
+DEVICE_CODENAME := panther
 DEVICE_PATH := device/google/pantah
-VENDOR_PATH := vendor/google/cheetah
+VENDOR_PATH := vendor/google/panther
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
-$(call inherit-product, device/google/gs201/lineage_common.mk)
-$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
+$(call inherit-product, device/google/gs201/pixelage_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-pixelage.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
-PRODUCT_MODEL := Pixel 7 Pro
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_MODEL := Pixel 7
+PRODUCT_NAME := pixelage_$(DEVICE_CODENAME)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="cheetah-user 15 BP1A.250305.019 13003188 release-keys" \
-    BuildFingerprint=google/cheetah/cheetah:15/BP1A.250305.019/13003188:user/release-keys \
+    BuildDesc="panther-user 15 BP1A.250305.019 13003188 release-keys" \
+    BuildFingerprint=google/panther/panther:15/BP1A.250305.019/13003188:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
